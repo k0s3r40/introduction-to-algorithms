fe8dc7dc8bdc751de3db71c1febbddb9d1a52db4
Kostadin Slavov <kostadin.slavov@tixi.bg>
2021-09-30 18:27:49 +0300

    Add increasing and decreasing option as in the book exercise

--- sorting/insert_sort.py
+++ sorting/insert_sort.py
@@ -1,14 +1,19 @@
-def insert_sort(arr):
+def insert_sort(arr, reverse=False):
     for j in range(1, len(arr)):
         key = arr[j]
         i = j - 1
-        while i >= 0 and arr[i] >= key:
-            arr[i + 1] = arr[i]
-            i -= 1
+        if reverse is False:
+            while i >= 0 and arr[i] >= key:
+                arr[i + 1] = arr[i]
+                i -= 1
+        else:
+            while i >= 0 and arr[i] <= key:
+                arr[i + 1] = arr[i]
+                i -= 1
         arr[i + 1] = key
-        print(arr)
+    return arr
 
 
 if __name__ == '__main__':
-    arr = [100, 5, 1, 5, 6, 1, 5, 4, 6, 7, 8, 9, 0, 10]
-    insert_sort(arr)
+    print("Increasing", insert_sort([100, 5, 1, 5, 6, 1, 5, 4, 6, 7, 8, 9, 0, 10]))
+    print("Decreasing", insert_sort([100, 5, 1, 5, 6, 1, 5, 4, 6, 7, 8, 9, 0, 10], reverse=True))
